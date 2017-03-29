386c55d7f3e5f56d545f5aff84c779cd7d5dc4f1
Travis <Travis>
2017-03-29 14:49:32 -0700

    Command-chaining, setter and getters.

--- laminar.typer.js
+++ laminar.typer.js
@@ -13,21 +13,51 @@ var Laminar = Laminar || {};
  * - the text to type
  * - the time between typed keys
  * ... maybe other stuff like frequency of errors (mistypes)
+ * 
+ * Use CSS on .laminarCursor to style the cursor.
+ * This makes a decent blinking cursor:
+ * 
+ 			.laminarCursor {
+				animation-name: blink;
+				animation-duration: 0.5s;
+				animation-iteration-count: infinite;
+			}
+			@keyframes blink {
+				0% {
+					opacity: 0
+				}
+				60% {
+					opacity: 0;
+				}
+				80% {
+					opacity: 1;
+				}
+				100% {
+					opacity: .25;
+				}
+			}
  */
 
 Laminar.Typer = (function() {
-  function Typer(domElementSelector,text,time,errorFreq,cursor) {
+  function Typer(domElementSelector,text,time,cursor,errorFreq) {
     if(domElementSelector===undefined || domElementSelector===null || domElementSelector==="") return false;
-    this.time = time || 200;
-    this.cursor = cursor || "&#9610;";
-    this.setCssStyle();
+    this.setTime(time);
+    this.setCursor(cursor);
     this.element = this.setTextField(document.querySelector(domElementSelector));
     this.setText(text);
+  }
+  Typer.prototype.setTime = function(time) {
+    this.time = time || 150;
+    return this;
+  }
+  Typer.prototype.setCursor = function(char) {
+    this.cursor = char || "&#9610;";
+    return this;
   }
   Typer.prototype.setText = function(text) {
     this.text = text || "This is Laminar.Typer!";
     this.count=0;
-    return true;
+    return this;
   }
   Typer.prototype.setTextField = function(element) {
     var textFields = element.querySelectorAll(".laminarTyper");
@@ -47,11 +77,12 @@ Laminar.Typer = (function() {
         that.stop();
       }
     },this.time);
-    return true;
+    return this;
   }
   Typer.prototype.stop = function() {
     document
     clearInterval(this.timer);
+    return this;
   }
   return Typer;
 })();
